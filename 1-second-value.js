681783b26ee20c8fdb394d7396df0099c46aca1d
root <root@vpl-1.asia-southeast1-b.c.vpl-gcs.internal>
2019-11-03 04:06:07 +0000

    js challenge 1

---
+++ 1-second-value.js
@@ -0,0 +1,33 @@
+/*  
+Problem description
+Given an array of numbers. Write program to return sencond lowest and second biggest numbers
+Sample:
+[3, 7, -6, 90, 100, 3, -6, 90, 10] => (3, 90)
+[1] => (1)
+[2, 3] => (2, 3)
+*/
+// Solution
+function secondValues(numbers) {
+    const asendingSortedNumbers = numbers.sort((a, b) => a - b);
+    const uniqueNumbers = [...new Set(asendingSortedNumbers)];
+
+    if(uniqueNumbers.length <= 0){
+        return "()";
+    }
+
+    if(uniqueNumbers.length === 1){
+        return `(${uniqueNumbers[0]})`;
+    }
+
+    if(uniqueNumbers.length === 2){
+        return `(${uniqueNumbers[0]},${uniqueNumbers[1]})`;
+    }
+
+    return `(${uniqueNumbers[1]},${uniqueNumbers[uniqueNumbers.length - 2]})`
+}
+
+console.log(secondValues([3, 7, -6, 90, 100, 3, -6, 90, 10]));
+console.log(secondValues([]));
+console.log(secondValues([2, 6, 8]));
+console.log(secondValues([6, 8]));
+console.log(secondValues([8]));
